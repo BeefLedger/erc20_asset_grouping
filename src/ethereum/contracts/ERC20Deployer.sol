aeb4111ccbd0c7abba2947f798f219bf832163ce
sdelvalle57 <sdelvalle57@gmail.com>
2021-01-21 11:08:44 -0500

    initial setup package

---
+++ src/ethereum/contracts/ERC20Deployer.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ERC20ForAssetGrouping.sol";
+
+
+contract ERC20Deployer is Ownable {
+
+    address[] public tokens;
+
+    function deploy(uint256 _chainId, string memory _name, string memory _symbol) public onlyOwner {
+        ERC20ForAssetGrouping uToken = new ERC20ForAssetGrouping(_chainId, _name, _symbol);
+        uToken.transferOwnership(msg.sender);
+        tokens.push(address(uToken));
+    }
+}
