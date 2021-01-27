93b744804a6e1b4f935a0fff5cabdc9870f13e51
sdelvalle57 <sdelvalle57@gmail.com>
2021-01-27 18:08:06 -0500

    get erc20 asset grouping contract values

--- src/chain/prefabContractFactory.ts
+++ src/chain/prefabContractFactory.ts
@@ -1,6 +1,6 @@
 import { Signer } from "ethers"
 
-import * as Erc20Compiled  from "../ethereum/abi/IERC20.json"
+import * as ERC20ForAssetGroupingCompiled  from "../ethereum/abi/ERC20ForAssetGrouping.json"
 import * as Erc20FactoryCompiled  from "../ethereum/abi/ERC20Factory.json"
 import { getContract } from "./contractFactory"
 import { ERC20ForAssetGrouping } from "../types/ERC20ForAssetGrouping"
@@ -11,5 +11,5 @@ export async function getERC20FactoryContract(address: string, signerIdxOrAddres
 }
 
 export async function getErc20Contract(address: string, signerIdxOrAddressOrSigner?: number | string | Signer): Promise<ERC20ForAssetGrouping> {
-    return getContract<ERC20ForAssetGrouping>(address, Erc20Compiled.abi, signerIdxOrAddressOrSigner)
+    return getContract<ERC20ForAssetGrouping>(address, ERC20ForAssetGroupingCompiled.abi, signerIdxOrAddressOrSigner)
 }
--- src/erc20Controller.ts
+++ src/erc20Controller.ts
@@ -10,37 +10,77 @@ export class ERC20Controller {
 
     constructor(erc20Address: string, signer: Signer) {
         this._erc20Address = erc20Address;
-        this._signer = signer
+        this._signer = signer;
     }
 
     public async balanceOf(): Promise<BigNumberish> {
         const contract = await this._getERC20Contract();
-        return contract.balanceOf(await this._signer.getAddress())
+        return contract.balanceOf(await this._signer.getAddress());
     }
 
+    public async getName(): Promise<string> {
+        const contract = await this._getERC20Contract();
+        return contract.name();
+    }
+
+    public async getSymbol(): Promise<string> {
+        const contract = await this._getERC20Contract();
+        return contract.symbol();
+    }
+
+    public async getChainId(): Promise<BigNumberish> {
+        const contract = await this._getERC20Contract();
+        return contract.chainId();
+    }
+
+    public async getGroupingContract(): Promise<string> {
+        const contract = await this._getERC20Contract();
+        return contract.assetGouping();
+    }
+
+    public async getTotalSupply(): Promise<BigNumberish> {
+        const contract = await this._getERC20Contract();
+        return contract.totalSupply();
+    }
+
+    public async getDecimals(): Promise<BigNumberish> {
+        const contract = await this._getERC20Contract();
+        return contract.decimals();
+    }
+ 
     public async transfer(recipient: string, amount: string): Promise<ContractReceipt> {
         const contract = await this._getERC20Contract();
-        return (await contract.functions.transfer(recipient, amount)).wait()
+        return (await contract.functions.transfer(recipient, amount)).wait();
     }
 
     public async transferOwnership(newOwner: string): Promise<ContractReceipt> {
         const contract = await this._getERC20Contract();
-        return  (await contract.functions.transferOwnership(newOwner)).wait()
+        return  (await contract.functions.transferOwnership(newOwner)).wait();
+    }
+
+    public async setGroupingContracts(address: string): Promise<ContractReceipt> {
+        const contract = await this._getERC20Contract();
+        return (await contract.functions.setAssetGrouping(address)).wait();
+    }
+
+    public async mint(receiver: string, amount: BigNumberish): Promise<ContractReceipt> {
+        const contract = await this._getERC20Contract();
+        return (await contract.functions.mint(receiver, amount)).wait();
     }
 
     private async _getERC20Contract(): Promise<ERC20ForAssetGrouping> {
         if(this._erc20Contract) {
-            return this._erc20Contract
+            return this._erc20Contract;
         }
 
         try {
             // Connect to the contract with my signer
-            const contract = await getErc20Contract(this._erc20Address, this._signer)
+            const contract = await getErc20Contract(this._erc20Address, this._signer);
             this._erc20Contract = contract;
-            return contract
+            return contract;
         }
         catch (e) {
-            throw Error(`Failed to get DealRoom contract: ${e}`)
+            throw Error(`Failed to get DealRoom contract: ${e}`);
         }
     }
 }
