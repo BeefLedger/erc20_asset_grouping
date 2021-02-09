87534de409c4194997a8eae7886972ee184a84ff
sdelvalle57 <sdelvalle57@gmail.com>
2021-02-09 14:05:09 -0500

    testing grouping contract

--- src/__tests__/grouping.ts
+++ src/__tests__/grouping.ts
@@ -5,6 +5,7 @@ import { ERC721Controller } from "../erc721Controller";
 import { MultisigController } from "../multisigController";
 import { GroupingController } from "../groupingController";
 import { ERC721BeefLedgerV11, Grouping, MultisigWalletV10 } from "../types";
+import { upgradeERC721 } from "../ethereum/deploy/deploy";
 
 
 let provider: ethers.providers.JsonRpcProvider;
@@ -30,6 +31,8 @@ describe("Grouping test", () => {
     const owner3 = await signers[2].getAddress(); 
     const owners = [owner1, owner2, owner3];
 
+
+    /** Multisig contract deployment */
     const [multisigContract, multisigProxyAddress]: [MultisigWalletV10, string] = await MultisigController.deployMultisigContract(signers[0], owners, 1);
     const multisigContractAddress = multisigContract.address;
     expect(multisigContractAddress.length).toEqual(42);
@@ -37,81 +40,49 @@ describe("Grouping test", () => {
     const multisig = new MultisigController(multisigContractAddress, signers[0]);
     const fetchedOwners = await multisig.getOwners();
     expect(fetchedOwners.length).toEqual(3);
+    ///////////////////////////////////////////////////
 
-    
+
+
+    /** ERC721 contract deployment */
     const [erc721Contract, erc721ProxyAddress]: [ERC721BeefLedgerV10, string] = await ERC721Controller.deployERC721Contract(signers[0], multisigContractAddress);
     const erc721ContractAddress = erc721Contract.address;
     expect(erc721ContractAddress.length).toEqual(42);
-
+    //mints tokens
+    const assets = [1, 2, 3, 4, 5]
     const erc721 = new ERC721Controller(erc721ContractAddress, signers[0]);
-    const contractTransaction  = await erc721.mint(owner1, [1, 2, 3, 4, 5]);
+    const contractTransaction  = await erc721.mint(owner1, assets);
     await contractTransaction.wait();
-
     let totalTokens = await erc721.getTotalTokens();
     expect(totalTokens.toString()).toEqual("5");
+    ///////////////////////////////////////////////////
 
-    const upgradedContract: ERC721BeefLedgerV11 = await erc721.upgradeERC721ContractV11(signers[0], erc721ProxyAddress);
-    totalTokens = await upgradedContract.getTotalTokens();
+
+
+    /** ERC721 contract upgrade */
+    const upgradedERC721: ERC721BeefLedgerV11 = await erc721.upgradeERC721ContractV11(signers[0], erc721ProxyAddress);
+    totalTokens = await upgradedERC721.getTotalTokens();
     expect(totalTokens.toString()).toEqual("5");
+    ///////////////////////////////////////////////////
 
+
+
+    /** Grouping contract deployment */
     const rgTokenAddress = "0x3A8E4Cc9deef5c4166f069245F715D15a6dA42ef"
-    const groupingContract: Grouping = await GroupingController.deployGroupingContract(signers[0], rgTokenAddress, upgradedContract.address);
+    const groupingContract: Grouping = await GroupingController.deployGroupingContract(signers[0], rgTokenAddress, upgradedERC721.address);
     const groupingContractAddress = groupingContract.address;
     expect(groupingContractAddress.length).toEqual(42);
+    ///////////////////////////////////////////////////
 
+
+    //Adds assets to grouping by calling addAssetsToGrouping from ERC721V11
+    (await upgradedERC721.functions.addAssetsToGrouping(assets, groupingContractAddress)).wait();
+    const groupingAddress = await upgradedERC721.getGroupingAddress(1);
+    expect(groupingAddress).toEqual(groupingContractAddress);
+    const getAssets = await groupingContract.getAssets();
+    expect(getAssets.length).toEqual(assets.length);
   })
 
-//   it("Deploys ERC20Token", async (): Promise<any> => {
-    
-//     const [erc20FactoryContract, proxyAddress]: [ERC20FactoryV10, string] = await ERC20FactoryController.deployERC20FactoryContract(signers[0]);
-//     const erc20ContractAddress = erc20FactoryContract.address;
-//     expect(erc20ContractAddress.length).toEqual(42);
-
-//     const erc20Factory = new ERC20FactoryController(erc20ContractAddress, signers[0])
-    
-//     const contractTransaction = await erc20Factory.deployERC20Contract(15122, "RG05", "RG05");
-//     const contractReceipt = await contractTransaction.wait();
-
-//     await provider.waitForTransaction(contractReceipt.transactionHash);
-//     const newERC20Event = contractReceipt.events.filter(evt => evt.event === 'NewERC20')
-//     if (newERC20Event) { 
-//         if (newERC20Event.length > 0) {
-//             if (newERC20Event.length === 1) {
-//                 const [erc20Address, owner] = (newERC20Event[0].args as [string, string])
-//                 const signerAddress = await signers[0].getAddress()
-//                 expect(owner).toEqual(signerAddress);
-
-//                 const allTokens = await erc20Factory.getTokens();
-//                 expect(allTokens[0].toLowerCase()).toEqual(erc20Address.toLowerCase())
-                
-//             } else {
-//                 console.log("aca1")
-//                 expect(true).toEqual(false)
-//             }
-//         }
-//     } else {
-//         console.log("aca2")
-//         expect(true).toEqual(false)
-//     } 
-//   })
-
-//   it("Upgrades ERC20Factory", async (): Promise<any> => {
-//     const [erc20FactoryContract, proxyAddress]: [ERC20FactoryV10, string] = await ERC20FactoryController.deployERC20FactoryContract(signers[0]);
-//     const erc20ContractAddress = erc20FactoryContract.address;
-//     expect(erc20ContractAddress.length).toEqual(42);
-
-//     const erc20Factory = new ERC20FactoryController(erc20ContractAddress, signers[0])
-    
-//     await erc20Factory.deployERC20Contract(15122, "RG05", "RG05");
-
-//     const upgradedContract: ERC20FactoryV11 = await erc20Factory.upgradeERC20FactoryContractV11(signers[0], proxyAddress);
-//     const testvariable = await upgradedContract.functions.getTestVariable();
-//     expect(testvariable.toString()).toEqual("745");
-    
-//     const allTokens = await upgradedContract.getTokens();
-//     expect(allTokens.length).toEqual(1)
-
-//   })
 })
 
     
